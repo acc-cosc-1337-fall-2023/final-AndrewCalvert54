6e2255515aec1ea1070cc7e121488128a04778d3
AndrewCalvert54 <143145146+AndrewCalvert54@users.noreply.github.com>
2023-12-08 21:09:58 +0000

    Question 2

--- src/roll.h
+++ src/roll.h
@@ -1,1 +1,23 @@
-//
+#ifndef ROLL_H
+#define ROLL_H
+
+#include "die.h"
+
+class Roll {
+public:
+    // Constructor
+    Roll(Die& d1, Die& d2);
+
+    // Public member functions
+    void roll_die();
+    int roll_value() const;
+
+private:
+    // Private member variables
+    Die& die1; // Initialize in Roll constructor
+    Die& die2; // Initialize in Roll constructor
+    bool rolled{false}; // Initialize to false
+    int value; // Set to die1 + die2 rolled values
+};
+
+#endif
--- test/craps_test.cpp
+++ test/craps_test.cpp
@@ -1,6 +1,7 @@
 #define CATCH_CONFIG_MAIN  // This tells Catch to provide a main() - only do this in one cpp file
 #include "catch.hpp"
 #include "die.h"
+#include "roll.h"
 
 TEST_CASE("Verify Test Configuration", "verification") {
 	REQUIRE(true == true);
@@ -14,6 +15,21 @@ TEST_CASE("Verify Die Class", "[Die]") {
             int value = die.rolled_value();
             REQUIRE(value >= 1); // Assert value is greater than or equal to 1
             REQUIRE(value <= 6); // Assert value is less than or equal to 6
+        }
+    }
+}
+
+TEST_CASE("Verify Roll Class", "[Roll]") {
+    SECTION("RollValueInRange") {
+        Die die1;
+        Die die2;
+        Roll roll(die1, die2);
+
+        for (int i = 0; i < 10; ++i) {
+            roll.roll_die();
+            int value = roll.roll_value();
+            REQUIRE(value >= 2); // Assert value is greater than or equal to 2
+            REQUIRE(value <= 12); // Assert value is less than or equal to 12
         }
     }
 }
